0fcf8d78fd8966784f18ff0f35defbde98ba3de9
patricio salfati <p.salfati@gmail.com>
2022-11-10 15:05:00 +0100

    planetdef

--- swapi.py
+++ swapi.py
@@ -4,3 +4,9 @@ import requests
 def import_planets():
     pr = requests.get('https://swapi.dev/api/planets/').json()
     return pr
+
+def planets_df():
+    tr = pd.DataFrame(import_planets["results"])
+    return tr
+    
+
