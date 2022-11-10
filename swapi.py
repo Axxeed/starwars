facab21862785bdfe9f27908934484b5a2a632ba
alexisgagneux <gagneux.alexis@gmail.com>
2022-11-10 13:48:10 +0100

    vaisseau en json

--- swapi.py
+++ swapi.py
@@ -4,3 +4,8 @@ import requests
 def import_vaisseau():
     r = requests.get("https://swapi.dev/api/starships")
     return r
+
+def vaisseau_json():
+    r = requests.get("https://swapi.dev/api/starships")
+    jsonr = r.json()
+    return jsonr
